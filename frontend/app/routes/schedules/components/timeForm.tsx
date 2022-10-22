66eb4f24110bd6f499353df09474c94211411b12
Mads Steenfeldt-Foss <mads.steenfeldt.foss@gmail.com>
2022-10-22 15:58:27 +0200

    fix timeform

--- frontend/app/routes/schedules/components/timeForm.tsx
+++ frontend/app/routes/schedules/components/timeForm.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import type {TimeWindow} from "~/routes/schedules/types";
-import {Input} from "@chakra-ui/input";
 import {Button} from "@chakra-ui/react";
 import {SmallAddIcon, SmallCloseIcon} from "@chakra-ui/icons";
 
@@ -15,9 +14,9 @@ const TimeForm = ({window, handleRemove, handleAdd}: TimeFormProps) => {
         <>
             <div className="flex flex-row items-baseline">
                 <label className="mr-1">From</label>
-                <Input style={{maxWidth: "5rem"}} name="from" defaultValue={window[0].slice(0, 5)}/>
+                <input type='time' name="from" defaultValue={window[0].slice(0, 5)}/>
                 <label className="mx-1">To</label>
-                <Input style={{maxWidth: "5rem"}} name="to" defaultValue={window[1].slice(0, 5)}/>
+                <input type='time' name="to" defaultValue={window[1].slice(0, 5)}/>
                 <Button className="mx-1" size="sm" variant="outline" type="button" onClick={() => {
                     handleRemove();
                 }}>
