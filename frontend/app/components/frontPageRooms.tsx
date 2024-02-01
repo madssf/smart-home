ac0e35b17ba18daecd1360b8ae1c988ec8d45bcd
Mads Steenfeldt-Foss <mads.steenfeldt.foss@gmail.com>
2024-02-01 14:29:24 +0100

    refactor frontpage rooms

---
+++ frontend/app/components/frontPageRooms.tsx
@@ -0,0 +1,101 @@
+import {EnrichedRoomData} from "~/routes/types";
+import {Alert, AlertDescription} from "~/components/ui/alert";
+import {Link} from "@remix-run/react";
+import {routes} from "~/routes";
+import {Badge} from "~/components/ui/badge";
+import {formatNumber} from "~/utils/formattingUtils";
+import dayjs from "dayjs";
+
+type Props = {
+    rooms: EnrichedRoomData[]
+    hideUnscheduledRooms: boolean
+}
+const FrontPageRooms = ({rooms, hideUnscheduledRooms}: Props) => {
+    if (rooms.length === 0) {
+        const message = hideUnscheduledRooms ? 'No rooms with schedule' : 'No rooms added yet';
+        return <Alert className="mt-2">
+            <AlertDescription>
+                {message}
+            </AlertDescription>
+        </Alert>;
+    } else {
+        return rooms.sort((a, b) => a.name.localeCompare(b.name)).map((room) => {
+            return<FrontPageRoom key={room.id} room={room}/>
+        });
+    }
+};
+
+const FrontPageRoom = ({room}: { room: EnrichedRoomData }) => {
+    return (
+        <div className="ml-1 mb-1">
+            <div className="flex flex-row items-center">
+                <Link
+                    className="text-xl font-bold cursor-pointer"
+                    to={routes.TEMP_LOG.ROOM_ID(room.id)}
+                >
+                    {room.name}
+                </Link>
+                {
+                    room.temp &&
+                    <div className="ml-2 grid grid-cols-[65px_auto] gap-1 p-1 items-baseline">
+                        <Badge className="text-left w-max text-md">
+                            {`${formatNumber(room.temp.temp, 1, 1)} °C`}
+                        </Badge>
+                        <p className={"ml-3"}>{dayjs(room.temp.time).fromNow()}</p>
+                    </div>
+                }
+            </div>
+            <div className="ml-1">
+                <div className="grid grid-cols-[70px_auto] gap-1 p-1">
+                    <p>Schedule</p>
+                    {room.activeSchedule?.schedule && room.activeSchedule.temp ?
+                        <Badge variant="secondary" className="text-left w-max">
+                            {`${formatNumber(room.activeSchedule.temp, 1, 1)} °C`}
+                        </Badge>
+                        : <Badge
+                            className="max-w-max ml-1"
+                            variant="outline"
+                        >
+                            Off
+                        </Badge>
+                    }
+                </div>
+                {
+                    room.plugStatuses.length > 0 &&
+                    <div className="ml-1">
+                        <p>Plugs</p>
+                        {
+                            room.plugStatuses.sort((a, b) => a.name.localeCompare(b.name)).map((plugStatus) => {
+                                return (
+                                    <div key={plugStatus.name} className="grid grid-cols-[100px_auto] gap-1 p-1">
+                                        <p>{plugStatus.name}</p>
+                                        <Badge
+                                            className="max-w-max ml-1"
+                                            variant={
+                                                plugStatus.is_on === null || plugStatus.power === null ?
+                                                    'destructive' :
+                                                    plugStatus.is_on ? 'default' : 'secondary'
+                                            }
+                                        >
+                                            {
+                                                plugStatus.is_on === null || plugStatus.power === null ?
+                                                    'ERROR'
+                                                    :
+                                                    plugStatus.is_on ?
+                                                        `${formatNumber(plugStatus.power, 1, 1)} W`
+                                                        : 'OFF'
+                                            }
+                                        </Badge>
+                                    </div>
+                                );
+                            })
+                        }
+                    </div>
+                }
+
+            </div>
+        </div>
+    );
+}
+
+export default FrontPageRooms;
--- frontend/app/routes/index.tsx
+++ frontend/app/routes/index.tsx
@@ -9,20 +9,19 @@ import {
     getPlugStatuses,
     getRoomTemps,
 } from "~/routes/index.server";
-import {Link, Links, Meta, Scripts, useLoaderData, useRouteError} from "@remix-run/react";
+import {Links, Meta, Scripts, useLoaderData, useRouteError} from "@remix-run/react";
 import type {Consumption, EnrichedRoomData, PriceInfo} from "./types";
-import React, {useState} from "react";
+import {useState} from "react";
 import ConsumptionGraph from "~/components/consumptionGraph";
 import type {LiveConsumptionChange, LiveConsumptionData} from "~/routes/liveData";
 import {fromSseEvent} from "~/routes/liveData";
 import {ClientOnly} from "remix-utils/client-only";
-import {formatNumber, formatPriceInfo} from "~/utils/formattingUtils";
+import {formatPriceInfo} from "~/utils/formattingUtils";
 import dayjs from "dayjs";
 
 import relativeTime from "dayjs/plugin/relativeTime.js";
 import LiveConsumptionGraph from "~/components/liveConsumptionGraph";
 import {getRooms} from "~/routes/rooms/rooms.server";
-import {routes} from "~/routes";
 import type {SimpleResult} from "~/fetcher/fetcher.server";
 import {Alert, AlertDescription} from "~/components/ui/alert";
 import {Badge} from "~/components/ui/badge";
@@ -31,6 +30,7 @@ import {Switch} from "~/components/ui/switch";
 import {Theme, useTheme} from "remix-themes";
 import {getErrorComponent} from "~/components/error";
 import {useEventSource} from "remix-utils/sse/react";
+import FrontPageRooms from "~/components/frontPageRooms";
 
 interface ResponseData {
     rooms: EnrichedRoomData[],
@@ -93,91 +93,6 @@ export default function Index() {
             return rooms.filter(r => r.plugStatuses.some(p => p.scheduled));
         }
         return rooms;
-    };
-
-    const renderRooms = (rooms: EnrichedRoomData[]) => {
-        if (rooms.length === 0) {
-            const message = hideUnscheduledRooms ? 'No rooms with schedule' : 'No rooms added yet';
-            return <Alert className="mt-2">
-                <AlertDescription>
-                    {message}
-                </AlertDescription>
-            </Alert>;
-        } else {
-            return rooms.sort((a, b) => a.name.localeCompare(b.name)).map((room) => {
-                return <React.Fragment key={room.id}>
-                    {renderRoomData(room)}
-                </React.Fragment>;
-            });
-        }
-    };
-
-    const renderRoomData = (room: EnrichedRoomData) => {
-        return (
-            <div className="ml-1 mb-1">
-                <div className="flex flex-row items-center">
-                    <Link to={routes.TEMP_LOG.ROOM_ID(room.id)}>{room.name}</Link>
-                    {
-                        room.temp &&
-                        <div className="ml-2 grid grid-cols-[65px_auto] gap-1 p-1">
-                            <Badge className="text-left w-max text-md">
-                                {`${formatNumber(room.temp.temp, 1, 1)} °C`}
-                            </Badge>
-                            <p className={"ml-3"}>{dayjs(room.temp.time).fromNow()}</p>
-                        </div>
-                    }
-                </div>
-                <div className="ml-1">
-                    <div className="grid grid-cols-[70px_auto] gap-1 p-1">
-                        <p>Schedule</p>
-                        {room.activeSchedule?.schedule && room.activeSchedule.temp ?
-                            <Badge variant="secondary" className="text-left w-max">
-                                {`${formatNumber(room.activeSchedule.temp, 1, 1)} °C`}
-                            </Badge>
-                            : <Badge
-                                className="max-w-max ml-1"
-                                variant="outline"
-                            >
-                                Off
-                            </Badge>
-                        }
-                    </div>
-                    {
-                        room.plugStatuses.length > 0 &&
-                        <div className="ml-1">
-                            <p>Plugs</p>
-                            {
-                                room.plugStatuses.sort((a, b) => a.name.localeCompare(b.name)).map((plugStatus) => {
-                                    return (
-                                        <div key={plugStatus.name} className="grid grid-cols-[100px_auto] gap-1 p-1">
-                                            <p>{plugStatus.name}</p>
-                                            <Badge
-                                                className="max-w-max ml-1"
-                                                variant={
-                                                plugStatus.is_on === null || plugStatus.power === null ?
-                                                    'destructive' :
-                                                    plugStatus.is_on ? 'default' : 'secondary'
-                                                }
-                                            >
-                                                {
-                                                    plugStatus.is_on === null || plugStatus.power === null ?
-                                                        'ERROR'
-                                                        :
-                                                    plugStatus.is_on ?
-                                                        `${formatNumber(plugStatus.power, 1, 1)} W`
-                                                        : 'OFF'
-                                                }
-                                            </Badge>
-                                        </div>
-                                    );
-                                })
-                            }
-                        </div>
-                    }
-
-                </div>
-            </div>
-        );
     };
 
     const consumptionGraphData = liveConsumptionData?.liveConsumption;
@@ -283,10 +198,7 @@ export default function Index() {
                             />
                         </div>
                     </div>
-
-                    {
-                        renderRooms(roomsToRender(data.rooms))
-                    }
+                    <FrontPageRooms rooms={roomsToRender(data.rooms)} hideUnscheduledRooms={hideUnscheduledRooms}/>
                 </div>
             </div>
         </div>
